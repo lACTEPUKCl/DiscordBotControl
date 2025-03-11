b93e086015197b684a2d7cca76a7b5f2e0152414
ACTEPUKC <makakakakala@gmail.com>
2025-03-11 09:18:42 +0200

    update

--- commands/restart.js
+++ commands/restart.js
@@ -7,7 +7,7 @@ const restartCommand = new SlashCommandBuilder()
   .setName("restart")
   .setDescription("Рестарт сервера")
   .setDefaultMemberPermissions(PermissionFlagsBits.SendTTSMessages);
-let servers = [];
+let server = [];
 let name;
 
 const execute = async (interaction) => {
@@ -15,11 +15,11 @@ const execute = async (interaction) => {
     await interaction.deferReply({ ephemeral: true });
 
     if (interaction.guildId === process.env.CIS) {
-      servers = [`custom-2`];
+      server = [`custom-2`];
       name = "CIS";
     }
     if (interaction.guildId === process.env.M1E) {
-      servers = [`m1e-1`];
+      server = [`m1e-1`];
       name = "M1E";
     }
     if (interaction.guildId === process.env.RNS) {
@@ -36,62 +36,60 @@ const execute = async (interaction) => {
           }
         }
       }
-      servers = [folder];
+      server = [folder];
       name = folder;
     }
 
-    if (servers.length > 0) {
-      for (const server of servers) {
-        console.log(`Запуск команды: docker compose down ${server}`);
-        const down = spawn("/usr/bin/docker", ["compose", "down", server], {
-          cwd: "/root/servers",
-        });
+    if (server.length > 0) {
+      console.log(`Запуск команды: docker compose down ${server}`);
+      const down = spawn("/usr/bin/docker", ["compose", "down", server], {
+        cwd: "/root/servers",
+      });
 
-        down.stdout.on("data", (data) => {
-          console.log(`[down stdout]: ${data.toString()}`);
-        });
-        down.stderr.on("data", (data) => {
-          console.error(`[down stderr]: ${data.toString()}`);
-        });
+      down.stdout.on("data", (data) => {
+        console.log(`[down stdout]: ${data.toString()}`);
+      });
+      down.stderr.on("data", (data) => {
+        console.error(`[down stderr]: ${data.toString()}`);
+      });
 
-        down.on("close", (code) => {
-          console.log(`Команда down завершилась с кодом ${code}`);
-          if (code === 0) {
-            console.log(
-              `Запуск команды: docker compose --env-file ./custom/.env up ${server}`
-            );
-            const up = spawn("/usr/bin/docker", ["compose", "up", server], {
-              cwd: "/root/servers",
-            });
+      down.on("close", (code) => {
+        console.log(`Команда down завершилась с кодом ${code}`);
+        if (code === 0) {
+          console.log(
+            `Запуск команды: docker compose --env-file ./custom/.env up ${server}`
+          );
+          const up = spawn("/usr/bin/docker", ["compose", "up", server], {
+            cwd: "/root/servers",
+          });
 
-            up.stdout.on("data", (data) => {
-              const message = data.toString();
-              console.log(`[up stdout]: ${message}`);
-              if (message.includes("LogEOSSessionListening")) {
-                interaction.editReply({
-                  content: `Сервер ${name} успешно перезагружен!`,
-                });
-                up.stdout.off("data");
-              }
-            });
-            up.stderr.on("data", (data) => {
-              console.error(`[up stderr]: ${data.toString()}`);
-            });
-            up.on("close", (code) => {
-              console.log(`Команда up завершилась с кодом ${code}`);
-              if (code !== 0) {
-                interaction.editReply({
-                  content: `Ошибка при запуске сервера ${name}.`,
-                });
-              }
-            });
-          } else {
-            interaction.editReply({
-              content: `Ошибка при остановке сервера ${name}.`,
-            });
-          }
-        });
-      }
+          up.stdout.on("data", (data) => {
+            const message = data.toString();
+            console.log(`[up stdout]: ${message}`);
+            if (message.includes("LogEOSSessionListening")) {
+              interaction.editReply({
+                content: `Сервер ${name} успешно перезагружен!`,
+              });
+              up.stdout.off("data");
+            }
+          });
+          up.stderr.on("data", (data) => {
+            console.error(`[up stderr]: ${data.toString()}`);
+          });
+          up.on("close", (code) => {
+            console.log(`Команда up завершилась с кодом ${code}`);
+            if (code !== 0) {
+              interaction.editReply({
+                content: `Ошибка при запуске сервера ${name}.`,
+              });
+            }
+          });
+        } else {
+          interaction.editReply({
+            content: `Ошибка при остановке сервера ${name}.`,
+          });
+        }
+      });
     } else {
       interaction.editReply({
         content: `Неизвестный сервер. Перезагрузка не выполнена.`,
