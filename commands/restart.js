ecae4d99383398b0debc0a22800a0f0d66055b56
ACTEPUKC <makakakakala@gmail.com>
2025-03-11 09:12:41 +0200

    update

--- commands/restart.js
+++ commands/restart.js
@@ -42,39 +42,43 @@ const execute = async (interaction) => {
 
     if (servers.length > 0) {
       for (const server of servers) {
-        const down = spawn(
-          "/usr/bin/docker",
-          ["compose", "--env-file", "./custom/.env", "down", server],
-          {
-            cwd: "/root/servers",
-          }
-        );
+        console.log(`Запуск команды: docker compose down ${server}`);
+        const down = spawn("/usr/bin/docker", ["compose", "down", server], {
+          cwd: "/root/servers",
+        });
+
+        down.stdout.on("data", (data) => {
+          console.log(`[down stdout]: ${data.toString()}`);
+        });
+        down.stderr.on("data", (data) => {
+          console.error(`[down stderr]: ${data.toString()}`);
+        });
 
         down.on("close", (code) => {
+          console.log(`Команда down завершилась с кодом ${code}`);
           if (code === 0) {
-            const up = spawn(
-              "/usr/bin/docker",
-              ["compose", "--env-file", "./custom/.env", "up", server],
-              {
-                cwd: "/root/servers",
-              }
+            console.log(
+              `Запуск команды: docker compose --env-file ./custom/.env up ${server}`
             );
+            const up = spawn("/usr/bin/docker", ["compose", "up", server], {
+              cwd: "/root/servers",
+            });
 
-            const handleUpData = (data) => {
+            up.stdout.on("data", (data) => {
               const message = data.toString();
-
+              console.log(`[up stdout]: ${message}`);
               if (message.includes("LogEOSSessionListening")) {
                 interaction.editReply({
                   content: `Сервер ${name} успешно перезагружен!`,
                 });
-
-                up.stdout.off("data", handleUpData);
+                up.stdout.off("data");
               }
-            };
-
-            up.stdout.on("data", handleUpData);
-
+            });
+            up.stderr.on("data", (data) => {
+              console.error(`[up stderr]: ${data.toString()}`);
+            });
             up.on("close", (code) => {
+              console.log(`Команда up завершилась с кодом ${code}`);
               if (code !== 0) {
                 interaction.editReply({
                   content: `Ошибка при запуске сервера ${name}.`,
